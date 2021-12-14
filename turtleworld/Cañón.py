ae1b6a94c183556f1c45786df2c7351dcf05a151
Rafa Rodríguez Galván <rafael.rodriguez@uca.es>
2021-12-14 10:38:40 +0100

    Añado la clase Cañón

---
+++ turtleworld/Cañón.py
@@ -0,0 +1,6 @@
+class Cañón(object):
+    def __init__(self, distancia):
+        self.distancia = distancia
+
+    def print_info(self):
+        print(f"Cañon con distancia de diparo: {self.distancia}")
